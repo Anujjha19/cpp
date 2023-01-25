5896f0d1ebae6ac77b44c97dd56de53d6cb29e79
Anuj Jha <anujjha710@gmail.com>
2023-01-25 23:23:00 +0530

    Boundary Traversal

--- B_Boundary_Traversal.cpp
+++ B_Boundary_Traversal.cpp
@@ -72,3 +72,11 @@ public:
         
         return ans;
     }
+
+Time Complexity: O(N).
+
+Reason: The time complexity will be O(H) + O(H) + O(N) which is ≈ O(N)
+
+Space Complexity: O(N)
+
+Reason: Space is needed for the recursion stack while adding leaves. In the worst case (skewed tree), space complexity can be O(N).
