8f2bdf5bf5c4ad28cd3de672698420815687653d
Anuj Jha <anujjha710@gmail.com>
2023-01-27 12:10:05 +0530

    Right View of Binary Tree

---
+++ B_Right_view_of_Tree.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    
+    void solve( Node* root , vector<int> &ans , int level){
+    //BaseCase
+    if(root ==NULL){
+        return;
+        
+    }
+    
+    //TO check new level  - [ we entered into a new level ]
+    
+    if( level == ans.size()) {
+        ans.push_back(root -> data );
+        
+    }
+    
+    solve( root -> right , ans , level + 1);
+        solve( root->left  , ans , level +1);
+
+    
+}
+
+    vector<int> rightView(Node *root)
+    {
+       // Your Code here
+        
+   vector<int> ans;
+   solve( root , ans , 0);
+   
+   return ans;
+    }
+};
